42caab99f62a26bec74d3fe352a39cd577c634f6
kdrcnrbyrm <36641435+kdrcnrbyrm@users.noreply.github.com>
2018-02-19 21:54:49 +0300

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,23 @@
+# Hello World program
+
+#!bin/python3
+
+#Insert code below this line to output Hello World
+print ("Hello World")
+
+
+#Declare a variable that has your name as the value
+name="Kadir"
+
+
+#Output Hello followed by your name
+print("hello",name)
+
+
+#Accept name from the user and store it in a variable
+name=input("Enter your name:")
+yas=input("yasinizi girin:")
+print("yasiniz",yas)
+
+#Output Hello followed by the user-defined name
+print("hello", name)
